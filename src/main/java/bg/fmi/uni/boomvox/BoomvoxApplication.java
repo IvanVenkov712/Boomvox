7f2af348f3594c0cecdde2615a9a1f74bddbb102
IvanVenkov712 <ivan.venkov.developer@gmail.com>
2026-05-09 14:31:24 +0300

    Created with Spring Initializr

---
+++ src/main/java/bg/fmi/uni/boomvox/BoomvoxApplication.java
@@ -0,0 +1,13 @@
+package bg.fmi.uni.boomvox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoomvoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoomvoxApplication.class, args);
+	}
+
+}
