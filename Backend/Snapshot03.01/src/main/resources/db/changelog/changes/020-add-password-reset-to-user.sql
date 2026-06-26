6587d0a3bdd6285933ddfdac58b2410e170185c4
IvanVenkov712 <ivan.venkov.developer@gmail.com>
2026-06-26 14:06:26 +0300

    Small fixes

--- Backend/Snapshot03.01/src/main/resources/db/changelog/changes/020-add-password-reset-to-user.sql
+++ Backend/Snapshot03.01/src/main/resources/db/changelog/changes/020-add-password-reset-to-user.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
 -- changeset boomvox:020
-ALTER TABLE user
+ALTER TABLE "user"
     ADD COLUMN reset_token VARCHAR(255) NULL,
-    ADD COLUMN reset_token_expiry DATETIME NULL;
+    ADD COLUMN reset_token_expiry TIMESTAMP(6) NULL;
