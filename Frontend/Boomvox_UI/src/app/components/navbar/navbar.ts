583d73d5c0c0d50c445b6c16274fd188f0ac29b6
Mihail <mihail5212@abv.bg>
2026-06-28 02:09:30 +0300

    Removing the default css and adding the correct one

--- Frontend/Boomvox_UI/src/app/components/navbar/navbar.ts
+++ Frontend/Boomvox_UI/src/app/components/navbar/navbar.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
